79cee686bfa49670595097de5a6fea277e2ac6b8
Thais Cailet <thais.cailet@servermonkey.com>
2018-03-15 11:02:29 -0500

    Closes #10 #11 adds dynamic train row to scheduler

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,6 @@
 var database = firebase.database();
+var trains = null;
+
 var utils = {
   varescape: function(str){
       return str.replace(/[^\w\s]/gi, '').replace(/\s/g, '_').toLowerCase();
@@ -35,26 +37,49 @@ function formatUserData(formdata){
     var trainobj = formdata;
     //http://momentjs.com/docs/#/displaying/
     //Non-unix e.g. format 2018-03-15T01:30:00-05:00 DATE|T|TIME-TIMEZONE
-    console.log(moment(trainobj.startime, 'HH:mm A').format());
     trainobj.startime = moment(trainobj.startime, 'HH:mm A').format('X'); //add fix for date set in the past later
     trainobj.name = utils.varescape(trainobj.name);
     //addTrain(trainobj);
-    calcMinutes(trainobj);
-    var nextarrival;
-    var minutesaway;
 }
 
 function calcMinutes(train){
 
     var difference = moment().diff(moment.unix(train.startime), 'minutes'); // positive if in past, negative if in future
-    var minutesAway = train.frequency - difference % train.frequency;
-    var nextArrival = moment().add(minutesAway, "m").format("hh:mm: A");
-    console.log(minutesAway, nextArrival);
+    var offset = train.frequency - difference % train.frequency;
+    var arrives = moment().add(offset, "m").format("hh:mm A");
+    train['nextarrival'] = arrives;
+    train['minutesaway'] = offset;
+    return train;
 
+}
+
+function displayRow(train)
+{
+    train = calcMinutes(train);
+    delete train.startime;
+    var $tr = $('<tr>').attr('id',train.name);
+    $.each(train, function(prop, value){
+        switch(prop){
+            default: $tr.append($('<td>').text(value));
+        }
+    });
+    $('#trains').append($tr);
+}
+
+function displayTrains(){
+    database.ref().on('child_added', function (snapshot, prevChildKey) {
+        if(snapshot.val() !== null){
+            var train = $.extend({'name': Object.keys(snapshot.val())[0]}, snapshot.val()[Object.keys(snapshot.val())[0]]);
+            displayRow(train);
+        }
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
 }
 
 $(document).ready(function(){
     setupParentNode();
+    displayTrains();
     $('#newtrain').submit(function(e) {
         e.preventDefault();
         var formdata = {};
